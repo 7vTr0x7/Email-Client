d44c096fbfd90f4a0ea03f72d542752494afad36
7vTr0x7 <7vTrox7@gmail.com>
2024-10-12 16:29:18 +0530

    Filters

--- frontend/src/components/Filters.jsx
+++ frontend/src/components/Filters.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Filters = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default Filters
+export default Filters;
