fde8c03e035d89cd6b73405a6460f9e587254e48
7vTr0x7 <7vTrox7@gmail.com>
2024-10-13 17:36:27 +0530

    Filter

--- frontend/src/components/Filters.jsx
+++ frontend/src/components/Filters.jsx
@@ -13,20 +13,26 @@ const Filters = () => {
 
   return (
     <section>
-      <div className="d-flex gap-4 fw-semibold pointer">
+      <div className="d-flex gap-4 fw-semibold ">
         <p>Filter By:</p>
         <p
-          className={`${filter === "unread" && "bg-filter  rounded-4 px-3"}`}
+          className={`${
+            filter === "unread" && "bg-filter  rounded-4 px-3 "
+          } pointer`}
           onClick={() => changeHandler("unread")}>
           Unread
         </p>
         <p
-          className={`${filter === "read" && "bg-filter  rounded-4 px-3"}`}
+          className={`${
+            filter === "read" && "bg-filter  rounded-4 px-3 "
+          } pointer`}
           onClick={() => changeHandler("read")}>
           Read
         </p>
         <p
-          className={`${filter === "favorites" && "bg-filter  rounded-4 px-3"}`}
+          className={`${
+            filter === "favorites" && "bg-filter  rounded-4 px-3 "
+          } pointer`}
           onClick={() => changeHandler("favorites")}>
           Favorites
         </p>
